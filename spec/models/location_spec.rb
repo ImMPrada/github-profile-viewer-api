9d11f14e9cb249bb77d21159c50909a483e62a09
immprada <im.mprada@gmail.com>
2022-08-04 18:15:39 -0500

    Add location model validations spec

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -4,4 +4,28 @@ RSpec.describe Location, type: :model do
   describe 'associations' do
     it { should have_many(:profiles) }
   end
+
+  describe 'profile model validations' do
+    let(:location) { create(:location) }
+    let(:duplicate_location) { create(:location) }
+
+    it 'should not be valid with blank or null name' do
+      wrong_location = location
+      wrong_location.name = ''
+      expect(wrong_location).not_to be_valid
+      wrong_location = location
+      wrong_location.name = nil
+      expect(wrong_location).not_to be_valid
+    end
+    it 'should not be valid if name is already takken' do
+      duplicate_location.name = location.name
+      expect(duplicate_location).not_to be_valid
+    end
+    it 'should not be case sensitive, at name uniqueness validation' do
+      duplicate_location.name = location.name.downcase
+      expect(duplicate_location).not_to be_valid
+      duplicate_location.name = location.name.upcase
+      expect(duplicate_location).not_to be_valid
+    end
+  end
 end
