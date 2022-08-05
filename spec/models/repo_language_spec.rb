08c2dcceafdce5ee24a55e90d65a9d41376a6775
immprada <im.mprada@gmail.com>
2022-08-04 23:10:39 -0500

    Add repo_language model validations spec

--- spec/models/repo_language_spec.rb
+++ spec/models/repo_language_spec.rb
@@ -5,4 +5,24 @@ RSpec.describe RepoLanguage, type: :model do
     it { should belong_to(:repo) }
     it { should belong_to(:language) }
   end
+
+  describe 'validations' do
+    subject(:repo_language) { create(:repo_language, repo: repo, language: language) }
+    let(:language) { create(:language) }
+    let(:repo) { create(:repo, profile: profile) }
+    let(:profile) { create(:profile, location: location) }
+    let(:location) { create(:location) }
+
+    it 'should be valid' do
+      expect(repo_language).to be_valid
+    end
+    it 'should not be valid with null repo' do
+      repo_language.repo = nil
+      expect(repo_language).not_to be_valid
+    end
+    it 'should not be valid with null language' do
+      repo_language.language = nil
+      expect(repo_language).not_to be_valid
+    end
+  end
 end
