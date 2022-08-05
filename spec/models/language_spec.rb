da0779d6e956a617ee59768663fc964aa75cd288
immprada <im.mprada@gmail.com>
2022-08-04 22:23:25 -0500

    Add language model validations spec

--- spec/models/language_spec.rb
+++ spec/models/language_spec.rb
@@ -7,4 +7,29 @@ RSpec.describe Language, type: :model do
     it { should have_many(:profile_languages) }
     it { should have_many(:profiles) }
   end
+
+  describe 'validations' do
+    subject(:language) { create(:language) }
+    subject(:language_duplicate) { create(:language) }
+
+    it 'should be valid' do
+      expect(language).to be_valid
+    end
+    it 'should not be valid with blank or null name' do
+      language.name = nil
+      expect(language).not_to be_valid
+    end
+    it 'should not be valid with blank or null amount' do
+      language.amount = nil
+      expect(language).not_to be_valid
+    end
+    it 'should not be valid if name is already takken' do
+      language_duplicate.name = language.name
+      expect(language_duplicate).not_to be_valid
+    end
+    it 'should not be case sensitive, at name uniqueness validation' do
+      language_duplicate.name = language.name.downcase
+      expect(language_duplicate).not_to be_valid
+    end
+  end
 end
