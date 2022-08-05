02e81452783ae44bf4853fa98db05e3538e8975c
immprada <im.mprada@gmail.com>
2022-08-04 22:35:19 -0500

    update language and repo models

--- app/models/language.rb
+++ app/models/language.rb
@@ -2,6 +2,8 @@ class Language < ApplicationRecord
   validates_presence_of :name
   validates_presence_of :amount
 
+  validates_uniqueness_of :name, :case_sensitive => false
+
   has_many :repo_languages, dependent: :destroy
   has_many :repos, through: :repo_languages
   has_many :profile_languages, dependent: :destroy
