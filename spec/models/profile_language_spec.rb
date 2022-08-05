4d16e1d54d2519e6f0d36736eba477272c859ea6
immprada <im.mprada@gmail.com>
2022-08-04 23:10:30 -0500

    Add profile_language model validations spec

--- spec/models/profile_language_spec.rb
+++ spec/models/profile_language_spec.rb
@@ -5,4 +5,23 @@ RSpec.describe ProfileLanguage, type: :model do
     it { should belong_to(:profile) }
     it { should belong_to(:language) }
   end
+
+  describe 'validations' do
+    subject(:profile_language) { create(:profile_language, profile: profile, language: language) }
+    let(:language) { create(:language) }
+    let(:profile) { create(:profile, location: location) }
+    let(:location) { create(:location) }
+
+    it 'should be valid' do
+      expect(profile_language).to be_valid
+    end
+    it 'should not be valid with null profile' do
+      profile_language.profile = nil
+      expect(profile_language).not_to be_valid
+    end
+    it 'should not be valid with null language' do
+      profile_language.language = nil
+      expect(profile_language).not_to be_valid
+    end
+  end
 end
