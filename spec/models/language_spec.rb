5824c397d96bf8a4aca160ac5bb87ec4c7f14c6a
immprada <im.mprada@gmail.com>
2022-08-06 02:06:50 -0500

    Update language model specs

--- spec/models/language_spec.rb
+++ spec/models/language_spec.rb
@@ -2,34 +2,28 @@ require 'rails_helper'
 
 RSpec.describe Language, type: :model do
   describe 'associations' do
-    it { should have_many(:repo_languages) }
-    it { should have_many(:repos) }
-    it { should have_many(:profile_languages) }
-    it { should have_many(:profiles) }
+    it { is_expected.to have_many(:repo_languages) }
+    it { is_expected.to have_many(:repos) }
+    it { is_expected.to have_many(:profile_languages) }
+    it { is_expected.to have_many(:profiles) }
   end
 
   describe 'validations' do
-    subject(:language) { create(:language) }
-    subject(:language_duplicate) { create(:language) }
+    subject(:language) { build(:language) }
 
-    it 'should be valid' do
-      expect(language).to be_valid
-    end
-    it 'should not be valid with blank or null name' do
-      language.name = nil
-      expect(language).not_to be_valid
-    end
-    it 'should not be valid with blank or null amount' do
-      language.amount = nil
-      expect(language).not_to be_valid
-    end
-    it 'should not be valid if name is already takken' do
-      language_duplicate.name = language.name
-      expect(language_duplicate).not_to be_valid
-    end
-    it 'should not be case sensitive, at name uniqueness validation' do
-      language_duplicate.name = language.name.downcase
-      expect(language_duplicate).not_to be_valid
-    end
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:amount) }
+  end
+
+  describe 'valid with amount = 0' do
+    subject(:language) { build(:language, amount: 0) }
+
+    it { expect(language).to be_valid }
+  end
+
+  describe 'valid with null amount' do
+    subject(:language) { build(:language, amount: nil) }
+
+    it { expect(language).not_to be_valid }
   end
 end
