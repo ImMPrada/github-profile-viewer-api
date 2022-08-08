621f39d3341cc9ce24c86e23df49d368caec8557
immprada <im.mprada@gmail.com>
2022-08-08 00:48:50 -0500

    add github api get

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,6 +1,12 @@
 module Api
   module V1
     class BaseController < Api::BaseController
+      BASER_URL = 'https://api.github.com'
+
+      def get_github_profile_data(user_name)
+        github_response = RestClient.get("#{BASER_URL}/users/#{user_name}")
+        { code: github_response.code, body: JSON.parse(github_response.body) }
+      end
     end
   end
 end
