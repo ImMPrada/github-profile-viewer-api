6da7e1b448c61836b66e6873a1c8f4db4347160b
immprada <im.mprada@gmail.com>
2022-08-07 20:14:49 -0500

    update repo spec

--- spec/models/repo_spec.rb
+++ spec/models/repo_spec.rb
@@ -2,36 +2,17 @@ require 'rails_helper'
 
 RSpec.describe Repo, type: :model do
   describe 'associations' do
-    it { should belong_to(:profile) }
-    it { should have_many(:repo_languages) }
-    it { should have_many(:languages) }
+    it { is_expected.to belong_to(:profile) }
+    it { is_expected.to have_many(:repo_languages) }
+    it { is_expected.to have_many(:languages) }
   end
 
   describe 'validations' do
-    subject(:repo) { create(:repo, profile: create(:profile, location: create(:location))) }
+    subject(:repo) { create(:repo) }
 
-    it 'should be valid' do
-      expect(repo).to be_valid
-    end
-    it 'should not be valid with blank or null name' do
-      repo.name = nil
-      expect(repo).not_to be_valid
-    end
-    it 'should not be valid with blank or null url' do
-      repo.url = nil
-      expect(repo).not_to be_valid
-    end
-    it 'should be valid with blank or null description' do
-      repo.description = nil
-      expect(repo).to be_valid
-    end
-    it 'should not be valid with blank or null languages_url' do
-      repo.languages_url = nil
-      expect(repo).not_to be_valid
-    end
-    it 'should not be valid with blank or null git_date' do
-      repo.git_date = nil
-      expect(repo).not_to be_valid
-    end
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:url) }
+    it { is_expected.to validate_presence_of(:languages_url) }
+    it { is_expected.to validate_presence_of(:git_date) }
   end
 end
