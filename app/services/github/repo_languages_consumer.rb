43e4bcb73a23ccb74848f8dccd8015afe2b46118
immprada <im.mprada@gmail.com>
2022-08-09 16:17:30 -0500

    Add repo languages consumer

---
+++ app/services/github/repo_languages_consumer.rb
@@ -0,0 +1,37 @@
+module Github
+  class RepoLanguagesConsumer
+    attr_accessor :repo_name, :github_repo_languages, :github_repo_languages_response_code
+
+    def initialize(user_name, repo_name)
+      github_response = ApiClient.new(user_name).get_github_repo_languages(repo_name)
+      self.github_repo_languages_response_code = github_response[:code]
+      self.github_repo_languages = github_response[:body]
+      self.repo_name = repo_name
+    end
+
+    def call
+      return nil unless github_repo_languages_response_code == 200
+
+      build_response
+    end
+
+    private
+
+    def build_response
+      languages = []
+      github_repo_languages.each do |language, amount| 
+        languages.push(build_languages_response(repo_name, language, amount))
+      end
+
+      languages
+    end
+
+    def build_languages_response(repo_name, language, amount)
+      {
+        repo: repo_name,
+        name: language,
+        amount: amount
+      }
+    end
+  end
+end
