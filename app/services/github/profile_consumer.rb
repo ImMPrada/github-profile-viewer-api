6b7a56cbf1cfec2ae94016c434648e8f99568f6b
immprada <im.mprada@gmail.com>
2022-08-11 18:30:29 -0500

    Upgrad ProfileConsumer

--- app/services/github/profile_consumer.rb
+++ app/services/github/profile_consumer.rb
@@ -1,17 +1,35 @@
 module Github
   class ProfileConsumer
-    attr_accessor :github_profile, :github_profile_response_code
+    attr_accessor :github_profile
 
-    GITHUB_RESPONSE_FIELDS = %w[login avatar_url html_url name company blog email bio twitter_username public_repos public_gists followers following updated_at location].freeze
-    PROFILE_FIELDS = %i[nickname avatar_url url name company blog email bio twitter_username public_repos public_gists followers following git_date location].freeze
+    FIELD_MAPPING = {
+      nickname: 'login',
+      avatar_url: 'avatar_url',
+      url: 'html_url',
+      name: 'name',
+      company: 'company',
+      blog: 'blog',
+      email: 'email',
+      bio: 'bio',
+      twitter_username: 'twitter_username',
+      public_repos: 'public_repos',
+      public_gists: 'public_gists',
+      followers: 'followers',
+      following: 'following',
+      git_date: 'updated_at',
+      location: 'location'
+    }.freeze
 
-    def initialize(user_name)
-      github_response = ApiClient.new(user_name).call_for_profile_data
-      self.github_profile_response_code = github_response[:code]
-      self.github_profile = github_response[:body]
+    def initialize(username)
+      @username = username
     end
 
     def call
+      github_response = ApiClient.new(@username)
+      github_response.fetch_profile_data
+      github_profile_response_code = github_response.code
+      self.github_profile = github_response.body
+
       return nil unless github_profile_response_code == 200
 
       build_response
@@ -21,8 +39,8 @@ module Github
 
     def build_response
       assambled_profile = {}
-      (0...GITHUB_RESPONSE_FIELDS.size).each do |i|
-        assambled_profile[PROFILE_FIELDS[i]] = github_profile[GITHUB_RESPONSE_FIELDS[i]]
+      FIELD_MAPPING.each do |profile_field, gh_field|
+        assambled_profile[profile_field] = github_profile[gh_field]
       end
       assambled_profile
     end
