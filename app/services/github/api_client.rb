3f546f244085d2f6885efa4e8f3712c11f4d10d5
immprada <im.mprada@gmail.com>
2022-08-11 15:39:35 -0500

    BASER_URL ------> BASE_URL

--- app/services/github/api_client.rb
+++ app/services/github/api_client.rb
@@ -2,22 +2,22 @@ module Github
   class ApiClient
     attr_accessor :user_name
 
-    BASER_URL = 'https://api.github.com'
+    BASE_URL = 'https://api.github.com'
 
     def initialize(user_name)
       self.user_name = user_name
     end
 
     def call_for_github_profile_data
-      get_data("#{BASER_URL}/users/#{user_name}")
+      get_data("#{BASE_URL}/users/#{user_name}")
     end
 
     def call_for_github_profile_repos
-      get_data("#{BASER_URL}/users/#{user_name}/repos")
+      get_data("#{BASE_URL}/users/#{user_name}/repos")
     end
 
     def get_github_repo_languages(repo_name)
-      get_data("#{BASER_URL}/repos/#{user_name}/#{repo_name}/languages")
+      get_data("#{BASE_URL}/repos/#{user_name}/#{repo_name}/languages")
     end
 
     private
