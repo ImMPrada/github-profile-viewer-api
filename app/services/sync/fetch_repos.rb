3b98619dd3ac1548399d3ff0497289cefec4436a
immprada <im.mprada@gmail.com>
2022-08-11 20:55:12 -0500

    upgrade Sync::(Fetch/Sync)Repos

--- app/services/sync/fetch_repos.rb
+++ app/services/sync/fetch_repos.rb
@@ -1,22 +1,23 @@
 module Sync
   class FetchRepos
-    attr_accessor :github_repos, :repos, :profile
-
     def initialize(profile)
       self.profile = profile
       self.repos = profile.repos
-      self.github_repos = Github::ReposConsumer.new(profile.nickname).call
     end
 
     def call
+      github_repos = Github::ReposConsumer.new(profile.nickname).call
+
       if repos.size.zero?
         github_repos.each do |github_repo|
           repo = Sync::SyncRepo.new(profile, github_repo).create_repo
-          Sync::FetchLanguages.new(profile, repo).call
+          # Sync::FetchLanguages.new(profile, repo).call
         end
-
-        profile.reload
       end
     end
+
+    private
+
+    attr_accessor :profile, :repos
   end
 end
--- app/services/sync/sync_repo.rb
+++ app/services/sync/sync_repo.rb
@@ -11,31 +11,23 @@ module Sync
       new_repo = Repo.new
       fetch_data new_repo
       fetch_profile new_repo
-      fetch_date new_repo
-
       new_repo.save
+
       new_repo
     end
 
     private
 
     def fetch_data(this_repo)
-      this_repo.name = github_repo[:name]
-      this_repo.url = github_repo[:url]
-      this_repo.description = github_repo[:description]
-      this_repo.is_active = true
+      github_repo[:is_active] = true
+      github_repo[:git_date] = Date.parse github_repo[:git_date]
+      this_repo.attributes = github_repo
 
       this_repo
     end
 
     def fetch_profile(this_repo)
       this_repo.profile = profile
-
-      this_repo
-    end
-
-    def fetch_date(this_repo)
-      this_repo.git_date = Date.parse github_repo[:git_date]
 
       this_repo
     end
