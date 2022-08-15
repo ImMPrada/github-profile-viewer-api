0e6c19fe4a9ada83ccb8ab43dfd8dd33c5503c05
immprada <im.mprada@gmail.com>
2022-08-15 15:20:00 -0500

    Implement Repo: create / update

--- app/services/sync/fetch_repos.rb
+++ app/services/sync/fetch_repos.rb
@@ -7,14 +7,35 @@ module Sync
 
     def call
       github_repos = Github::ReposConsumer.new(profile.nickname).call
-      github_repos.each do |github_repo|
-        repo = Sync::SyncRepo.new(profile, github_repo).create_repo
-        Sync::FetchLanguages.new(profile, repo).call
-      end
+      github_repos.each { |github_repo| syncronize_repo(github_repo) }
     end
 
     private
 
-    attr_accessor :profile, :repos
+    attr_accessor :profile, :repos, :repo
+
+    def syncronize_repo(github_repo)
+      self.repo = Repo.find_by(name: github_repo[:name])
+
+      create_repo_if_not_exists(github_repo)
+      syncronyze_repo(github_repo)
+    end
+
+    def create_repo_if_not_exists(github_repo)
+      return unless repo.nil?
+
+      self.repo = Sync::SyncRepo.new(profile, github_repo).create_repo
+      Sync::FetchLanguages.new(profile, repo).call
+    end
+
+    def syncronyze_repo(github_repo)
+      this_repo_date = repo.git_date
+      github_repo_date = Date.parse(github_repo[:git_date])
+
+      return unless this_repo_date < github_repo_date
+
+      Sync::SyncRepo.new(profile, github_repo).update_repo(repo)
+      Sync::FetchLanguages.new(profile, repo).call
+    end
   end
 end
