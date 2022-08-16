94460a16610216eaeafdb85fed93aa87188ea0b0
immprada <im.mprada@gmail.com>
2022-08-15 20:32:12 -0500

    delete validations section

--- spec/models/profile_language_spec.rb
+++ spec/models/profile_language_spec.rb
@@ -5,15 +5,4 @@ RSpec.describe ProfileLanguage, type: :model do
     it { is_expected.to belong_to(:profile) }
     it { is_expected.to belong_to(:language) }
   end
-
-  describe 'validations' do
-    subject(:profile_language) { create(:profile_language, profile: profile, language: language) }
-
-    let(:language) { create(:language) }
-    let(:location) { create(:location) }
-    let(:profile) { create(:profile, location: location) }
-
-    it { expect(profile_language.profile).to eq(profile) }
-    it { expect(profile_language.language).to eq(language) }
-  end
 end
--- spec/models/repo_language_spec.rb
+++ spec/models/repo_language_spec.rb
@@ -2,17 +2,7 @@ require 'rails_helper'
 
 RSpec.describe RepoLanguage, type: :model do
   describe 'associations' do
-    it { should belong_to(:repo) }
-    it { should belong_to(:language) }
-  end
-
-  describe 'validations' do
-    let(:repo_language) { create(:repo_language, repo: repo, language: language) }
-
-    let(:language) { create(:language) }
-    let(:repo) { create(:repo) }
-
-    it { expect(repo_language.repo).to eq(repo) }
-    it { expect(repo_language.language).to eq(language) }
+    it { is_expected.to belong_to(:repo) }
+    it { is_expected.to belong_to(:language) }
   end
 end
