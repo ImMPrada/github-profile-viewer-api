e1a1095788ea60fe042953fea7894e634a9aa2f4
immprada <im.mprada@gmail.com>
2022-08-17 17:33:09 -0500

    TOTATO refactor models: add class methods to language model. Remove initial instance methods from Profile and Repo models

--- app/models/repo.rb
+++ app/models/repo.rb
@@ -6,25 +6,4 @@ class Repo < ApplicationRecord
   belongs_to :profile
   has_many :repo_languages, dependent: :destroy
   has_many :languages, through: :repo_languages
-
-  def languages_with_weight
-    calc_languages_total_aamount
-    calc_languages_weight
-
-    wiegths
-  end
-
-  private
-
-  attr_accessor :languages_total_amount, :wiegths
-
-  def calc_languages_total_aamount
-    self.languages_total_amount = languages.pluck(:amount).sum.to_f
-  end
-
-  def calc_languages_weight
-    self.wiegths = {}
-
-    languages.each { |language| wiegths[language.name] = (language.amount.to_f / languages_total_amount).round(5) }
-  end
 end
