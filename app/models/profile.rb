aa619aaa6ae52bcc8ac6e766720352175c6efdca
immprada <im.mprada@gmail.com>
2022-08-17 16:27:53 -0500

    Add languages_with_weight method to Profile

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -13,4 +13,33 @@ class Profile < ApplicationRecord
   has_many :repos, dependent: :destroy
   has_many :profile_languages, dependent: :destroy
   has_many :languages, through: :profile_languages
+
+  def languages_with_weight
+    list_languages
+    calc_languages_total_aamount
+    calc_languages_weight
+
+    languages_weight
+  end
+
+  private
+
+  attr_accessor :languages_total_amount, :language_list, :languages_weight
+
+  def list_languages
+    self.language_list = languages.pluck(:name).uniq
+  end
+
+  def calc_languages_total_aamount
+    self.languages_total_amount = languages.pluck(:amount).sum.to_f
+  end
+
+  def calc_languages_weight
+    self.languages_weight = {}
+
+    language_list.each do |language|
+      language_total_amount = languages.where(name: language).sum(:amount).to_f
+      languages_weight[language] = (language_total_amount / languages_total_amount).round(5)
+    end
+  end
 end
