6c85c210c3ae976826aee4d356b33ee37f276343
immprada <im.mprada@gmail.com>
2022-08-19 07:48:14 -0500

    FetchProfile: When profile doesn't exist on DB but exists on github, should return the profile, and create it on the DB

--- spec/services/sync/fetch_profile_spec.rb
+++ spec/services/sync/fetch_profile_spec.rb
@@ -4,7 +4,25 @@ RSpec.describe Sync::FetchProfile do
   let(:profile_name) { 'immprada' }
   let(:profile_url) { 'https://github.com/ImMPrada' }
   let(:fetch_profile) { described_class.new(profile_name) }
-  # let(:prfile_consumer) { Github::ProfileConsumer.new(profile_name) }
+  let(:profile_data) do
+    {
+      nickname: 'ImMPrada',
+      avatar: 'https://avatars.githubusercontent.com/u/26731448?v=4',
+      name: 'miguel',
+      url: profile_url,
+      bio: nil,
+      blog: 'https://www.linkedin.com/in/immprada/',
+      company: nil,
+      email: nil,
+      followers_count: 10,
+      followings_count: 6,
+      git_date: '2022-08-03T00:16:27Z',
+      location: 'Colombia',
+      public_gists_count: 20,
+      public_repos_count: 60,
+      twitter: 'im_mprada'
+    }
+  end
 
   describe '#call' do
     before do
@@ -26,24 +44,7 @@ RSpec.describe Sync::FetchProfile do
     before do
       profile_response = instance_double(
         Github::ProfileConsumer,
-        call:
-          {
-            nickname: 'ImMPrada',
-            avatar: 'https://avatars.githubusercontent.com/u/26731448?v=4',
-            name: 'miguel',
-            url: profile_url,
-            bio: nil,
-            blog: 'https://www.linkedin.com/in/immprada/',
-            company: nil,
-            email: nil,
-            followers_count: 6,
-            followings_count: 6,
-            git_date: '2022-08-03T00:16:27Z',
-            location: 'Colombia',
-            public_gists_count: 0,
-            public_repos_count: 15,
-            twitter: 'im_mprada'
-          }
+        call: profile_data
       )
       allow(Github::ProfileConsumer).to receive(:new).and_return(profile_response)
       null_repo_consumer_response = instance_double(
@@ -51,15 +52,15 @@ RSpec.describe Sync::FetchProfile do
         call: nil
       )
       allow(Sync::FetchRepos).to receive(:new).and_return(null_repo_consumer_response)
-      fetch_profile.call
     end
 
-    it 'returns a profile, registered in DB' do
+    it 'saves profile data in DB' do
+      fetch_profile.call
       expect(Profile.find_by(nickname: profile_name)).not_to be_nil
     end
 
     it 'returns the right profile form DB' do
-      expect(Profile.find_by(nickname: profile_name)[:url]).to eq(profile_url)
+      expect(fetch_profile.call.url).to eq(profile_url)
     end
   end
 end
